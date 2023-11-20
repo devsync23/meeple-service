8ea7fbaa395a2b88f67ff4d9faed410934ffe1a3
therealjustinbiele <justin.biele@gmail.com>
2023-11-20 12:22:04 -0800

    feat: add users get and post routes, with middleware

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
 
 // express ROUTE
 // comprised of 3 things:
 // .METHOD() ---> HTTP method
 // endpoint aka path
 // handler aka controller
-app
-  .get('/', (req, res) => {
-    res.send('Hello team!')
+
+/** health check endpoints */
+app.route('/')
+  .get((req, res) => {
+    res.send('server is healthy')
   })
-  .post('/', (req, res) => {
+  .post((req, res) => {
     res.send('you sent a post request!')
   })
-  .put('/', (req, res) => {
+  .put((req, res) => {
     res.send('put request received')
   })
-  .delete('/', (req, res) => {
+  .delete((req, res) => {
     res.send('you sent a delete request. DELETE THIS!')
   })
+
+const users = [
+  {
+    id: 1,
+    name: "JB",
+    email: "jb@jb.com"
+  }
+]
+
+function handleGetUsers(req, res) {
+  res.send(users)
+}
+
+function validateUserPost(req, res, next) {
+  const newUser = req.body;
+  if (!newUser.email || !newUser.name) {
+    return res.status(400).send('missing fields from the middleware')
+  }
+  if (users.some((user) => user.email === newUser.email)) {
+    return res.status(400).send(`user with ${newUser.email} already exists, from the middleware`)
+  }
+  next()
+}
+
+function addUserToUsersList(req, res, next) {
+  console.log('next() called me!')
+  const newUser = req.body
+  users.push(newUser)
+  res.send(users)
+}
+
+app
+  .route('/users')
+    .get(handleGetUsers)
+    .post(validateUserPost, addUserToUsersList)
+  
+
 
 app.listen(port, () => {
   console.log('app is listening on port: ', port)
