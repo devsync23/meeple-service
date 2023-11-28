bd495fd0b90c2d7488c476ef8457093bb66a2f71
Samuel Lee <samuellee005@gmail.com>
2023-11-27 19:59:22 -0800

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const express = require('express')
+import express from 'express'
+import bodyParser from 'body-parser'
+import userRouter from './users/user.routes.js'
+
 const app = express()
 const port = 3000
 
-// express ROUTE
-// comprised of 3 things:
-// .METHOD() ---> HTTP method
-// endpoint aka path
-// handler aka controller
-app
-  .get('/', (req, res) => {
-    res.send('Hello team!')
-  })
-  .post('/', (req, res) => {
-    res.send('you sent a post request!')
-  })
-  .put('/', (req, res) => {
-    res.send('put request received')
-  })
-  .delete('/', (req, res) => {
-    res.send('you sent a delete request. DELETE THIS!')
-  })
+app.use(bodyParser.json())
+app.use('/users', userRouter)
 
 app.listen(port, () => {
   console.log('app is listening on port: ', port)
