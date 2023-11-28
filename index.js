f12480b37971ec42876fdb7f7de2c4c4b46178ab
Dan Wang <danwang625@gmail.com>
2023-11-27 19:55:01 -0800

    add register

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express')
+import express from 'express'
+import bodyParser from 'body-parser'
+import userRouter from './users/user.routes.js'
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+app.use('/users', userRouter)
 
 // express ROUTE
 // comprised of 3 things:
 // .METHOD() ---> HTTP method
 // endpoint aka path
 // handler aka controller
-app
-  .get('/', (req, res) => {
-    res.send('Hello team!')
-  })
-  .post('/', (req, res) => {
-    res.send('you sent a post request!')
-  })
-  .put('/', (req, res) => {
-    res.send('put request received')
-  })
-  .delete('/', (req, res) => {
-    res.send('you sent a delete request. DELETE THIS!')
-  })
+// app
+//   .get('/', (req, res) => {
+//     res.send('Hello team!')
+//   })
+//   .post('/', (req, res) => {
+//     res.send('you sent a post request!')
+//   })
+//   .put('/', (req, res) => {
+//     res.send('put request received')
+//   })
+//   .delete('/', (req, res) => {
+//     res.send('you sent a delete request. DELETE THIS!')
+//   })
 
 app.listen(port, () => {
   console.log('app is listening on port: ', port)
