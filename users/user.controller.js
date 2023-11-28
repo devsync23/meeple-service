c5d953e881984afc716535bb268912b26c53bbc9
therealjustinbiele <justin.biele@gmail.com>
2023-11-27 20:41:57 -0800

    feat: add fs

--- users/user.controller.js
+++ users/user.controller.js
@@ -1,3 +1,22 @@
+import fs from 'fs'
+
 export function registerUser(req, res) {
+  const newUserData = req.body
+  const formattedUserData = {
+    [newUserData.email]: {
+      name: newUserData.name,
+      password: newUserData.password,
+      age: newUserData.age,
+      verified: false,
+    }
+  }
+  fs.writeFileSync('./users/users.json', JSON.stringify(formattedUserData, null, 4))
+  
   res.send("you registered your user!")
+}
+
+export function userLogin(req, res) {
+  const body = req.body
+  console.log(req.body)
+  res.send({})
 }
--- users/user.routes.js
+++ users/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { registerUser } from './user.controller.js'
+import { userLogin, registerUser } from './user.controller.js'
 
 const router = express.Router()
 
-router.post('/register', registerUser);
+router.post('/register', registerUser)
+router.post('/login', userLogin)
 
 export default router
