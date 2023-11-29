db1489999952e6a67bf3eb531ce0b7b4debdf95e
Natalie Lei <50159668+natalei@users.noreply.github.com>
2023-11-28 18:14:58 -0800

    end of w3d2 231128 class

--- users/user.controller.js
+++ users/user.controller.js
@@ -1,8 +1,28 @@
+import fs from 'fs'
+
 export function registerUser(req, res) {
-    res.send("you registered your user")
+    // res.send("you registered your user")
+    const newUserData = req.body
+    const formattedUserData = {
+        [newUserData.email]: {
+            name: newUserData.name,
+            password: newUserData.password,
+            age: newUserData.age,
+            verified: false
+        }
+    }
+    // console.log(formattedUserData)
+    fs.writeFileSync('./users.json', JSON.stringify(formattedUserdata, null, 4))
+    // JSON.stringify(
+            // value,
+            // replacer - specifies properties of 'value' to be included in the input,
+            // space - insert white space into the output JSON string for readability)
+    res.send("you registered your user!")
 }
 
 export function loginUser(req, res) {
-    res.body
+    const body = req.body
+    console.log(req.body.email)
+    res.send({})
     // get the req data and save it to some variable
 }
