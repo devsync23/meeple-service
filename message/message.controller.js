0d1b962d8446f761654f82ebd170de13c4e191a2
Samuel Lee <samuellee005@gmail.com>
2023-12-04 18:56:52 -0800

    changes made

--- message/message.controller.js
+++ message/message.controller.js
@@ -3,7 +3,7 @@ import fs from "fs"
 export function getMessage(req, res) {
     let existingMessage = JSON.parse(fs.readFileSync('./message/messages.json'), 'utf8')
     console.log(existingMessage)
-    if(Object.keys(existingMessage).length === 0){
+    if (Object.keys(existingMessage).length === 0) {
         return res.send('Empty history')
     }
     res.send('Here is the message log history' + '\n' + JSON.stringify(existingMessage, null, 4))
@@ -21,5 +21,5 @@ export function createMessage(req, res) {
     }
     existingMessages = { ...existingMessages, ...formattedMessage }
     fs.writeFileSync('./message/messages.json', JSON.stringify(existingMessages, null, 4))
-    res.send(`We have received your message!\n`+ "Translating message:\n" + `"${message.text}"`)
+    res.send(`We have received your message!\n` + "Translating message:\n" + `"${message.text}"`)
 }
