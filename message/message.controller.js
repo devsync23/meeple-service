f19b8347affac6923d524ac930f8a6b221712f8a
Dan Wang <danwang625@gmail.com>
2023-12-04 22:55:22 -0800

    update authenticateUserMessages and validateUserMessages middleware function and message controller createMessage function'

--- message/message.controller.js
+++ message/message.controller.js
@@ -10,16 +10,25 @@ export function getMessage(req, res) {
 }
 
 export function createMessage(req, res) {
-    let existingMessages = JSON.parse(fs.readFileSync('./message/messages.json', 'utf8'))
-    console.log(existingMessages)
+    let existingUsers = JSON.parse(fs.readFileSync('./message/messages.json', 'utf8'))
+    console.log(existingUsers)
     const message = req.body;
     const formattedMessage = {
-        [message.text]: {
+        [message.user_email]: [{
+            user_id: message.user_email,
+            text: message.text,
             sourceLanguage: message.sourceLanguage,
-            targetLanguage: message.targetLanguage
-        }
+            targetLanguage: message.targetLanguage,
+            translation: message.translation,
+            createdAt: Date.now()
+        }]
     }
-    existingMessages = { ...existingMessages, ...formattedMessage }
-    fs.writeFileSync('./message/messages.json', JSON.stringify(existingMessages, null, 4))
-    res.send(`We have received your message!\n`+ "Translating message:\n" + `"${message.text}"`)
+    if (message.user_email in existingUsers) {
+        existingUsers[message.user_email].push(formattedMessage[message.user_email])
+    } else {
+        existingUsers[message.user_email] = formattedMessage[message.user_email]
+    }
+    fs.writeFileSync('./message/messages.json', JSON.stringify(existingUsers, null, 4))
+
+    res.send(`We have received your message!\n`+ "Translating message:\n" + `"${message.translation}"`)
 }
--- message/message.middleware.js
+++ message/message.middleware.js
@@ -1,14 +1,27 @@
 import fs from "fs"
 import jwt from 'jsonwebtoken'
 
-export async function authenticateUserMessages(req, res, next) {
+export function authenticateUserMessages(req, res, next) {
     console.log(req);
     const token = req.headers.authorization
-    const isVerified = await jwt.verify(token, "shhhhh")
-    console.log(isVerified)
+    try {
+        const userData = jwt.verify(token, "shhhhhh")
+        req.user = userData
+        console.log("userData>>>>>>>>>>:", userData)
+    } catch (err) {
+        return res.send(400, "oops, something's wrong, could not authenticate user")
+    }
     next()
 }
 
 export function validateUserMessages(req, res, next) {
-
+    if (!req.body.sourceLanguage
+        || !req.body.targetLanguage
+        || !req.body.text
+        ) {
+        res.send('data not valid')
+    }
+    // req.user = user_email
+    // console.log(user_email)
+    next()
 }
