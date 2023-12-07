bb2c6fd48b5a6dd6cd8a1e6bff16fcc574240d0b
Samuel Lee <samuellee005@gmail.com>
2023-12-06 18:19:04 -0800

    completed up to getMessages

--- messages/helperFunctions.js
+++ messages/helperFunctions.js
@@ -4,6 +4,8 @@ export function recentTranslations(input1, input2) {
     const userEmail = input2
     const existingUserEmail = input1[userEmail]
     const userEmailLength = existingUserEmail.length
+    userEmailLength === 2 ? counter = 2 :
+        userEmailLength === 1 ? counter = 1 : counter = 3;
 
     for (let i = userEmailLength - 1; i >= userEmailLength - counter; i--) {
         const recentMessage = {
