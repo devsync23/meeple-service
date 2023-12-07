fcfdb5c8b843e5dcb4082334cdd05b1d6af55580
therealjustinbiele <justin.biele@gmail.com>
2023-12-06 19:00:39 -0800

    fix: final bugs

--- messages/message.controller.js
+++ messages/message.controller.js
@@ -14,10 +14,11 @@ export async function createMessage(req, res) {
     let existingMessage = JSON.parse(fs.readFileSync('./messages/messages.json', 'utf8'))
     console.log(existingMessage)
     const message = req.body;
+    const { email } = req.user;
     const translatedText = await translator(`Please translate ${message.text} from ${message.sourceLanguage} to ${message.targetLanguage}`)
     const formattedMessage = {
-        [message.user_email]: [{
-            user_id: message.user_email,
+        [email]: [{
+            user_id: email,
             text: message.text,
             sourceLanguage: message.sourceLanguage,
             targetLanguage: message.targetLanguage,
@@ -25,10 +26,10 @@ export async function createMessage(req, res) {
             createdAt: Date.now()
         }]
     }
-    if (message.user_email in existingMessage) {
-        existingMessage[message.user_email].push(formattedMessage[message.user_email][0])
+    if (email in existingMessage) {
+        existingMessage[email].push(formattedMessage[email][0])
     } else {
-        existingMessage[message.user_email] = formattedMessage[message.user_email]
+        existingMessage[email] = formattedMessage[email]
     }
     fs.writeFileSync('./messages/messages.json', JSON.stringify(existingMessage, null, 4))
 
